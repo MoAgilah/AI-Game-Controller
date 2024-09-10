4281300469f87c1d03ed65fdd236a526d1b18a57
Mohamed agilah <mo22agilah@gmail.com>
2024-09-10 12:59:59 +0100

    Remove unused functions from world. Replace shared ptr with unique ptr due to previous changes to the collision manager.

--- 2DTestbed/Code/Game/World.h
+++ 2DTestbed/Code/Game/World.h
@@ -10,16 +10,11 @@
 enum class Texts { Name, Time, Max };
 enum class Sprites { Pipe1, Pipe2, Pipe3, Name, Time, Max};
 
-class Camera;
-
 class World
 {
 public:
 	World();
 	~World() = default;
-
-	const std::vector<std::shared_ptr<Enemy>>& GetEnemies() const { return m_enemies; }
-	const std::vector<std::shared_ptr<Object>>& GetObjects() const { return m_objects; }
 
 	void Update(float deltaTime);
 	void Render(sf::RenderWindow& window);
@@ -42,6 +37,6 @@ private:
 	AABB m_foregroundBox;
 	Sprite m_backgroundSpr;
 	std::array<Sprite, (int)Sprites::Max> m_sprites;
-	std::vector<std::shared_ptr<Enemy>> m_enemies;
-	std::vector<std::shared_ptr<Object>> m_objects;
+	std::vector<std::unique_ptr<Enemy>> m_enemies;
+	std::vector<std::unique_ptr<Object>> m_objects;
 };
