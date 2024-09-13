165b99969d681be96d66b4ceba13b2da3619899d
Mohamed agilah <mo22agilah@gmail.com>
2024-09-13 12:36:58 +0100

    remove enemies from the world.

--- 2DTestbed/Code/Game/World.cpp
+++ 2DTestbed/Code/Game/World.cpp
@@ -116,12 +116,12 @@ void World::AddEnemies()
 {
 	Tile* tmp;
 	/*tmp = GameManager::GetGameMgr()->GetCollisionMgr()->GetTile(13, 5);
-	m_enemies.push_back(std::make_unique<Koopa>(false, tmp->GetPosition()));*/
+	m_enemies.push_back(std::make_unique<Koopa>(false, tmp->GetPosition()));
 
 	tmp = GameManager::GetGameMgr()->GetCollisionMgr()->GetTile(32, 8);
 	m_enemies.push_back(std::make_unique<Bill>(false, tmp->GetPosition() + sf::Vector2f(0, 22)));
 
-	/*tmp = GameManager::GetGameMgr()->GetCollisionMgr()->GetTile(35, 11);
+	tmp = GameManager::GetGameMgr()->GetCollisionMgr()->GetTile(35, 11);
 	m_enemies.push_back(std::make_unique<Rex>(false, tmp->GetPosition()));
 
 	tmp = GameManager::GetGameMgr()->GetCollisionMgr()->GetTile(47, 11);
